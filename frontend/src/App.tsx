1ff6e1ea031013ea2f4faf06d490767be0793d5b
Jonas Danielius <jonas.danielius@mif.stud.vu.lt>
2019-05-07 19:47:21 +0300

    Revert "material design"
    This reverts commit 97e4779c0b07d9791a654278e29472db0b268438.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Login from './Login/Login';
-import Table from './Table/Table';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Login></Login>
-        <Table></Table>
       </div>
     );
   }
