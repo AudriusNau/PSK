97e4779c0b07d9791a654278e29472db0b268438
Jonas Danielius <jonas.danielius@mif.stud.vu.lt>
2019-05-07 19:41:55 +0300

    material design
    added material design framework and table

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Login from './Login/Login';
+import Table from './Table/Table';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Login></Login>
+        <Table></Table>
       </div>
     );
   }
