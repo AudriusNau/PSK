62c03df33c299b50d5de10d633692f45f305a798
Jonas Danielius <jonas.danielius@mif.stud.vu.lt>
2019-05-08 16:01:18 +0300

    router + employee pending travel table

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import './App.scss';
+import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import Login from './Login/Login';
-import Table from './Table/Table';
+import Table from './Tables/Table';
+import EmployeePendingTravelTable from './Tables/EmployeePendingTravelTable'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      /*<div className="App">
         <Login></Login>
         <Table></Table>
+      </div>*/
+      <Router>
+      <div>
+        <nav>
+          <Link to="/">Login </Link>
+          <Link to="/table">Table </Link>
+          <Link to="/employeePendingTravelTable">EmployeePendingTravelTable </Link>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/table" component={Table} />
+          <Route exact path="/employeePendingTravelTable" component={EmployeePendingTravelTable} />
+        </Switch>
       </div>
+    </Router>
     );
   }
 }
