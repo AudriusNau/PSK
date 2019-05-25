2c8d1be825edfd6e1fbbd5aa8b56f5be3d8780c9
yoannna <o.smitaite@gmail.com>
2019-05-25 20:39:25 +0300

    Show login error

--- Angular/src/app/Login/login.component.ts
+++ Angular/src/app/Login/login.component.ts
@@ -3,7 +3,7 @@ import { Router } from '@angular/router';
 import { HeaderComponent } from '../navigation/header/header.component';
 import { AuthenticateComponent } from './authenticate.component';
 import { Url } from '../http/url';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { User } from '../entities/user';
 
 @Component({
@@ -19,6 +19,8 @@ export class LoginComponent implements OnInit {
   authenticateComponent: AuthenticateComponent;
   user: User;
 
+  showError = false;
+
   ngOnInit() {
     this.headerComponent = new HeaderComponent();
     this.authenticateComponent = new AuthenticateComponent(this.router);
@@ -26,15 +28,20 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
+    this.showError = false;
     const username = (<HTMLInputElement> document.getElementById('username')).value;
     const password = (<HTMLInputElement> document.getElementById('password')).value;
 
-    this.http.get(Url.get('authentication/get/getEmployeeByUsernameAndPassword?username=' + username + '&password=' + password))
-    .subscribe((user: User) => {
+    this.http.get(Url.get('authentication/get/getEmployeeByUsernameAndPassword?username=' + username + '&password=' + password)).subscribe(
+      (user: User) => {
         this.user = user;
         console.log(user);
         console.log(this.user);
         this.authenticateComponent.login(this.user);
-    });
+      },
+      (error: HttpErrorResponse) => {
+        this.showError = true;
+      }
+    );
   }
 }
