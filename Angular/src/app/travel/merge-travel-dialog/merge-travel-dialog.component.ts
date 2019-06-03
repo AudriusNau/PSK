ceac2f1bf85d09626633cb9a4191e327eaa17282
Audrius <audrius.naujokas@gmail.com>
2019-06-03 21:39:27 +0300

    merge function not finished

--- Angular/src/app/travel/merge-travel-dialog/merge-travel-dialog.component.ts
+++ Angular/src/app/travel/merge-travel-dialog/merge-travel-dialog.component.ts
@@ -3,6 +3,8 @@ import {HttpClient} from "@angular/common/http";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
 import {Accommodation} from "../../entities/accommodation";
 import {Travel} from "../../entities/travel";
+import {Url} from "../../http/url";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-merge-travel-dialog',
@@ -11,11 +13,13 @@ import {Travel} from "../../entities/travel";
 })
 export class MergeTravelDialogComponent implements OnInit {
   selectedTravels: Array<Travel>
-
+  baseTravel: number
+  private rules: string = String('Rules:\n 1 dates should be similar (+- 1 day)\n 2 destination should be the same\n');
   constructor(
     private http: HttpClient,
     public dialogRef: MatDialogRef<MergeTravelDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Array<Travel>
+    @Inject(MAT_DIALOG_DATA) public data: Array<Travel>,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -25,5 +29,15 @@ export class MergeTravelDialogComponent implements OnInit {
     this.dialogRef.close();
   }
 
+  onMergeClick() {
+    this.http.put(
+      Url.get('travel/merge'),
+      {baseTravelId: this.baseTravel,
+        travels: this.selectedTravels.splice (this.baseTravel, 1).map(item => item.id)}
+    ).subscribe(response => {
+      if (response == null) {alert(' Does not match the rules\n' + this.rules); }
+      else {this.onCloseClick(); }
+    });
+  }
 
 }
--- Angular/src/app/travel/travel.component.ts
+++ Angular/src/app/travel/travel.component.ts
@@ -19,7 +19,6 @@ import {MergeTravelDialogComponent} from "./merge-travel-dialog/merge-travel-dia
 export class TravelComponent implements OnInit {
     items: Array<Travel> = [];
     selectedTravels: Array<Travel> = [];
-    private rules: string = String('Rules:\n 1 dates should be similar (+- 1 day)\n 2 destination should be the same\n');
     public displayedColumns: string[] = ['startDate', 'endDate', 'price', 'departureOffice', 'arrivalOffice', 'organiser', 'info'];
     constructor(private http: HttpClient, private userService: UserService, private router: Router, private dialog: MatDialog) { }
   ngOnInit() {
@@ -52,15 +51,6 @@ export class TravelComponent implements OnInit {
 
     goToDetails(id: number) {
         this.router.navigate(['/travel', id]);
-    }
-    merge() {
-      this.http.put(
-        Url.get('travel/merge'),
-        {baseTravelId: this.selectedTravels[0].id, travels: this.selectedTravels.splice (1, 1).map(item => item.id)}
-        ).subscribe(response => {
-          if (response == null) {alert('Offices does not match\n' + this.rules); }
-      else {this.loadTable(), error1 => alert('Dates does not match the rules\n ' + this.rules); }
-         });
     }
     openDialog(): void {
       this.selectedTravels = this.items.filter(item => item.isSelected === true);
