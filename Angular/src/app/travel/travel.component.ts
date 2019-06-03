af1c67661cd8ab68e526acd928afab4dc405bd08
Audrius <audrius.naujokas@gmail.com>
2019-06-03 14:27:05 +0300

    merge alerts improved

--- Angular/src/app/travel/travel.component.ts
+++ Angular/src/app/travel/travel.component.ts
@@ -55,8 +55,11 @@ export class TravelComponent implements OnInit {
       this.http.put(
         Url.get('travel/merge'),
         {baseTravelId: this.selectedTravels[0].id, travels: this.selectedTravels.splice (1, 1).map(item => item.id)}
-        ).subscribe(() => this.loadTable(), error1 => alert("Can not merge travels\n " +
-        "Rules:\n 1 dates should be similar (+- 1 day)\n 2 destination should be the same"));
+        ).subscribe(response => {
+          if (response == null) {alert('Offices does not match'); }
+      else {this.loadTable(), error1 => alert('Dates does not match the rules\n ' +
+        'Rules:\n 1 dates should be similar (+- 1 day)\n 2 destination should be the same'); }
+         });
     }
 }
 
