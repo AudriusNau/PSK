be3e5e3e4bd71e727d7d5a7827ac2569ec2097f3
Audrius <audrius.naujokas@gmail.com>
2019-06-03 14:48:08 +0300

    alert bad merging

--- Angular/src/app/travel/travel.component.ts
+++ Angular/src/app/travel/travel.component.ts
@@ -17,6 +17,7 @@ import {UserService} from "../services/user.service";
 export class TravelComponent implements OnInit {
     items: Array<Travel> = [];
     selectedTravels: Array<Travel> = [];
+    private rules: string = String('Rules:\n 1 dates should be similar (+- 1 day)\n 2 destination should be the same\n');
     public displayedColumns: string[] = ['startDate', 'endDate', 'price', 'departureOffice', 'arrivalOffice', 'organiser', 'info'];
     constructor(private http: HttpClient, private userService: UserService, private router: Router) { }
   ngOnInit() {
@@ -56,9 +57,8 @@ export class TravelComponent implements OnInit {
         Url.get('travel/merge'),
         {baseTravelId: this.selectedTravels[0].id, travels: this.selectedTravels.splice (1, 1).map(item => item.id)}
         ).subscribe(response => {
-          if (response == null) {alert('Offices does not match'); }
-      else {this.loadTable(), error1 => alert('Dates does not match the rules\n ' +
-        'Rules:\n 1 dates should be similar (+- 1 day)\n 2 destination should be the same'); }
+          if (response == null) {alert('Offices does not match\n' + this.rules); }
+      else {this.loadTable(), error1 => alert('Dates does not match the rules\n ' + this.rules); }
          });
     }
 }
